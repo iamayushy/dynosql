6348a760e7768de2261a794e989c8fed4d3b5904
Ayush Yadav <52492154+iamayushy@users.noreply.github.com>
2022-07-01 20:24:51 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <>
     <Paper style={{marginTop:'-1.4rem', backgroundColor:'#111111'}}>
 
-      <h3 style={{color:'blue', padding:'1.3rem', fontSize:'1.4rem'}}>Atlan Text Editor</h3>
+      <h3 style={{color:'blue', padding:'1.3rem', fontSize:'1.4rem'}}>The Simple SQL Editor</h3>
     </Paper>
     <WorkDesk/>
     </>
